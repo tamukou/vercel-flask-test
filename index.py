313882b0e578af2258adcafbe1b8b43beb8f6f6a
tamukou <46435319+tamukou@users.noreply.github.com>
2022-06-21 01:38:31 +0900

    fix

--- index.py
+++ index.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-   return "<p>Hello, World!</p>"
+    return jsonify({'message': 'This is Poke API !'})
+
+
+@app.route('/api/<id>', methods=["GET"])
+def id(id):
+    id = id * 2
+    return jsonify({'id': id})  
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8888, debug=True)
+
