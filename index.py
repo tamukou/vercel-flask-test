49d16e02acdb22e51d1a69f292f6a89773233d24
tamukou <46435319+tamukou@users.noreply.github.com>
2022-06-21 01:50:54 +0900

    fix

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, requests, json
 
 app = Flask(__name__)
 
@@ -10,9 +10,31 @@ def hello_world():
 
 @app.route('/api/<id>', methods=["GET"])
 def id(id):
-    id = id * 2
-    return jsonify({'id': id})  
+    #pokeapiから取得
+    name = get_name(id)
 
+    # json_open = open('pokemon.json', 'r')
+    # json_load = json.load(json_open)
+    # print(json_load['section1']['key'])
+    # id = id * 2
+    return jsonify({'id': id, 'name': name})  
+
+
+def get_name(id_str):
+    url = "https://pokeapi.co/api/v2/pokemon/" + id_str  + "/"
+    r = requests.get(url, timeout=5)
+    r = r.json()
+    name  = r['name']
+    id    = r['id']
+    image = r['sprites']['front_default']
+    types = r['types'][0]['type']['name']
+
+    print(id)
+    print(name)
+    print(image)
+    print(types)
+
+    return name
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8888, debug=True)
